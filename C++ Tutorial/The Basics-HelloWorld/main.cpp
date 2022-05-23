5156499a61c56409085629d69c20f658c0e19e49
Liang <SulaFelinae@gmail.com>
2022-05-23 14:23:29 -0700

    set up

---
+++ C++ Tutorial/The Basics-HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  The Basics-HelloWorld
+//
+//  Created by Liang Zhao on 5/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
