8e4a7b945e494fa0769d3a9aca5fc0c6530fac4c
Liang <SulaFelinae@gmail.com>
2022-08-17 22:32:46 -0700

    readfile

--- C++ Tutorial/the Basics-WriteFile/main.cpp
+++ C++ Tutorial/the Basics-WriteFile/main.cpp
@@ -36,36 +36,4 @@ int main()
   return 0 ;
     
 }
-#include <fstream>
-#include <string>
-#include <iostream>
-using namespace std ;
-
-int main()
-{
-    
-  string poem = "\n\tI never saw a man who looked" ;
-  poem.append( "\n\tWith such a wistful eye" ) ;
-  poem.append( "\n\tUpon that little tent of blue" ) ;
-  poem.append( "\n\tWhich prisoners call the sky" ) ;
-
-//Mac has weird file permissions
-// How to fix?
-// Using FILE SYSTEM.H
-  ofstream writer( "poem.txt" ) ;
-
-  if( ! writer )
-  {
-    cout << "Error opening file for output" << endl ;
-    return -1 ;
-  }
-  
-  writer << poem << endl ;
-  
-  writer.close() ;
-
-  return 0 ;
-    
-}
-
 
