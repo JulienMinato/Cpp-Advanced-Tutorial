7e68b813a60014daa81a111d25485c6659076bd2
Liang <SulaFelinae@gmail.com>
2022-08-19 08:41:40 -0700

    except

---
+++ C++ Tutorial/the Basics-Except/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  the Basics-Except
+//
+//  Created by Liang Zhao on 8/19/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
