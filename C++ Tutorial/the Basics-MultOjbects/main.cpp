a32d813e6b922a93b5917a479b5c650df411e5c1
Liang <SulaFelinae@gmail.com>
2022-08-18 21:36:24 -0700

    MultObj

--- C++ Tutorial/the Basics-MultOjbects/main.cpp
+++ C++ Tutorial/the Basics-MultOjbects/main.cpp
@@ -5,10 +5,59 @@
 //  Created by Liang Zhao on 8/18/22.
 //
 
+#include <string>
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+class Dog
+{
+  int age, weight ;
+  string color ;
+
+public:
+  void bark() { cout << "WOOF!" << endl ; }
+
+  //void setAge( int yrs ) { age = yrs ; }
+  //void setWeight( int lbs ) { weight = lbs ; }
+  //void setColor( string hue ) { color = hue ; }
+
+  void setValues( int, int, string ) ; // Prototype.
+
+  int getAge() { return age; }
+  int getWeight() { return weight; }
+  string getColor() { return color; }
+} ;
+
+void Dog::setValues( int age, int weight, string color ) // Definition.
+{
+  this -> age = age ;
+  this -> weight = weight ;
+  this -> color = color ;
+}
+
+int main()
+{
+  Dog fido ;
+
+  //fido.setAge( 3 ) ;
+  //fido.setWeight( 15 ) ;
+  //fido.setColor( "brown" ) ;
+
+  fido.setValues( 3, 15, "brown" ) ;
+
+  cout << "Fido is a " << fido.getColor() << " dog" << endl ;
+  cout << "Fido is " << fido.getAge() << " years old" << endl ;
+  cout << "Fido weighs " << fido.getWeight() << " pounds" << endl ;
+
+  fido.bark() ;
+
+  Dog pooch ;
+  pooch.setValues( 4, 18, "gray" ) ;
+  cout << "Pooch is a " << pooch.getAge() ;
+  cout << " year old " << pooch.getColor() ;
+  cout << " dog who weighs " << pooch.getWeight() ;
+  cout << " pounds." ;
+  pooch.bark() ;
+
+  return 0 ;
 }
