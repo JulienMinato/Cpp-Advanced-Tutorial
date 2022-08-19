564699907396139a6f4a92012b2d6393109c33da
Liang <SulaFelinae@gmail.com>
2022-08-18 21:35:31 -0700

    Obj

---
+++ C++ Tutorial/the Basics-MultOjbects/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  the Basics-MultOjbects
+//
+//  Created by Liang Zhao on 8/18/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
