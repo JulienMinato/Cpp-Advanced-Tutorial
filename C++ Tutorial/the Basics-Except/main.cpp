c461ab75efbf443fea700bfbabbde50fb7a1e2d1
Liang <SulaFelinae@gmail.com>
2022-08-19 08:42:02 -0700

    Handling Errors

--- C++ Tutorial/the Basics-Except/main.cpp
+++ C++ Tutorial/the Basics-Except/main.cpp
@@ -5,10 +5,43 @@
 //  Created by Liang Zhao on 8/19/22.
 //
 
+#include <stdexcept>
+#include <fstream>
+#include <string>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <typeinfo>
+
+using namespace std;
+
+int main()
+{
+  string lang =  "C++" ;
+  int num = 1000000000; // One billion.
+
+  try
+  {
+    lang.replace( 100, 1, "C" ) ; // 1. out_of_range exception
+
+    //lang.resize( 3 * num ); // 2. length error
+
+    //ifstream reader( "nonsuch.txt" ) ;
+    //if( ! reader ) throw logic_error("File not found" ); // 3. logic_error
+  }
+  catch( out_of_range &e )
+  {
+    cerr << "Range Exception: " << e.what() << endl ;
+    cerr << "Exception Type: " << typeid( e ).name() ;
+    cout << endl << "Program terminated." << endl ;
+    return -1 ;
+  }
+  catch( exception &e )
+  {
+    cerr << "Exception: " << e.what() << endl ;
+    cerr << "Exception Type: " << typeid( e ).name() << endl ;
+  }
+  
+  cout << "Program continues..." << endl ;
+
+  return 0 ;
 }
