56c4d4b7b0162d65802d35b74027cbe592718654
Liang <SulaFelinae@gmail.com>
2022-08-21 16:46:02 -0700

    macro done

--- C++ Tutorial/the Basics-MacroFunctions/main.cpp
+++ C++ Tutorial/the Basics-MacroFunctions/main.cpp
@@ -5,10 +5,24 @@
 //  Created by Liang Zhao on 8/21/22.
 //
 
-#include <iostream>
+#define SQUARE(n) ( n * n )
+#define CUBE(n) ( n * n * n )
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <iostream>
+using namespace std ;
+
+inline int square( int n ) { return ( n * n ); }
+inline int cube( int n ) { return ( n * n * n ); }
+
+int main()
+{
+  int num = 5 ;
+
+  cout << "Macro SQUARE: " << SQUARE( num ) << endl ;
+  cout << "Inline square: " << square( num ) << endl ;
+
+  cout << "Macro CUBE: " << CUBE( num ) << endl ;
+  cout << "Inline cube: " << cube( num ) << endl ;
+
+  return 0 ;
 }
