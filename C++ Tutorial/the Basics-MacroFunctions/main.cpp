b1594319364be0f0652e310352d9b244195de82c
Liang <SulaFelinae@gmail.com>
2022-08-21 16:45:21 -0700

    macro

---
+++ C++ Tutorial/the Basics-MacroFunctions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  the Basics-MacroFunctions
+//
+//  Created by Liang Zhao on 8/21/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
