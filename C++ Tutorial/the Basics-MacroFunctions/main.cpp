248ec07b5d365d2b7428fc170269d29f63c14bf3
Liang <SulaFelinae@gmail.com>
2022-08-23 21:30:34 -0700

    rec 2

--- C++ Tutorial/the Basics-MacroFunctions/main.cpp
+++ C++ Tutorial/the Basics-MacroFunctions/main.cpp
@@ -52,3 +52,44 @@ int main()
 
   return 0 ;
 }
+
+
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+  int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } ;
+
+  // Pointer assigns address of element zero.
+  int* ptr = nums ;
+
+  cout << endl << "ptr at: " << ptr << " gets: " << *ptr ;
+
+  // Increment to element one.
+  ptr++ ;
+  cout << endl << "ptr at:\t" << ptr << " gets: " << *ptr  ;
+
+  // Increment again to element two.
+  ptr++ ;
+  cout << endl << "ptr at: " << ptr << " gets: " << *ptr ;
+
+  // Decrement again to element zero.
+  ptr -= 2 ;
+  cout << endl << "ptr at: " << ptr << " gets: " << *ptr ;
+  
+  // (Added for formatting.)
+  cout << endl ;
+
+  for( int i = 0 ; i < 10 ; i++ )
+  {
+    cout << endl << "Element: " << i ;
+    cout << "    Value: " << *ptr ;
+    ptr++ ;
+  }
+
+  // (Added for formatting).
+  cout << endl << endl ;
+
+  return 0 ;
+}
