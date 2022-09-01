c02324eb8f7c6cf451d594c7937776024da76517
Liang <SulaFelinae@gmail.com>
2022-08-31 21:49:50 -0700

    vec

--- C++ Tutorial/EssentialOperations/main.cpp
+++ C++ Tutorial/EssentialOperations/main.cpp
@@ -1,45 +1,36 @@
-#include <iostream>
-#include <vector>
+#include "vector.h"
 
-using std::cout;
-using std::endl;
-
-// Text based on the book: Stephen Prata, "C++ Primer Plus", 6th edition (2012), chapter 18; and
-// Bjarne Stroustrup, "A Tour of C++", 2nd edition (2018).
+#include <list>
 
 
-void print_vector(const std::vector<int> v) {
-    for (int i = 0; i < v.size(); i++)
-        cout << " " << v[i];
-    cout << "\n";
+template <typename T>
+Vector<T>::Vector(int size) {
+    std::cout << "ordinary constructor for Vector\n";
+    
+    if (size < 0)
+        throw std::length_error("Vector constructor: negative size");
+    
+    size_ = size;
+    elem_ = new T[size];
 }
 
 
-
-int main() {
-    std::vector<int> v = {1, 2, 3, 4, 5};
-    
-    int sum = 0;
-    int mult = 1;
-    
-    // A lambda function can access/capture any automatic variable in its scope.
-    
-    cout << "vector of x values\n";
-    print_vector(v);
-    
-    // [&] : capture all local names used in the lambda function by reference
-    std::for_each(v.begin(), v.end(), [&](const int& x) { sum += x; mult *= x; });
-    cout << "sum = " << sum << "\n";
-    cout << "mult = " << mult << "\n";
-    
-    // [&name1, &name2, ...] : capture the variables name1, name2, ... by reference
-    sum = 0;
-    mult = 1;
-    std::for_each(v.begin(), v.end(), [&sum, &mult](const int& x) { sum += x; mult *= x; });
-    cout << "sum = " << sum << "\n";
-    cout << "mult = " << mult << "\n";
-    
-    
-    return 0;
+template<typename T>
+T& Vector<T>::operator[](int i) const {
+    return elem_[i];
 }
 
+// adding some possible template classes for Vector
+template class Vector<int>;
+template class Vector<char>;
+template class Vector<double>;
+template class Vector<std::list<int>>;
+
+
+template<typename T>
+void foo(T& val) {
+    std::cout << "val = " << val << "\n";
+}
+
+// adding template classes to the template function foo
+template void foo<int>(int& val);
