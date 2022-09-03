38505869822476330b330e49313b6a866607887f
Liang <SulaFelinae@gmail.com>
2022-09-02 19:35:59 -0700

    iterators

--- C++ Tutorial/EssentialOperations/main.cpp
+++ C++ Tutorial/EssentialOperations/main.cpp
@@ -1,28 +1,65 @@
 #include <iostream>
+#include <vector>
+#include <list>
 
-// We can define our own simple range algorithms for our custom containers.
-// For example, we can easily provide the shorthand to say just `sort(v)` instead of `sort(v.begin(), v.end())`
+using std::cout;
+using std::cerr;
+using std::string;
+using std::vector;
+using std::list;
+
+
+// We can generalize the function find_all()
 //
-// Bjarne Stroustrup puts the container versions of `sort()` (and other algorithms) into their own namespace Estd (“extended std”)
-// to avoid interfering with other programmers’ uses of namespace std and also to make it easier to replace this stopgap with Ranges.
+// The typename is needed to inform the compiler that C ’s iterator is supposed to be a
+// type and not a value of some type, say, the integer 7
+//
+// We can hide this implementation detail by introducing a type alias
 
-namespace Estd {
-    using namespace std;
+template<typename T>
+using Iterator = typename T::iterator; // T's iterator
+
+template<typename C, typename V>
+vector<Iterator<C>> find_all(C& c, V v) {
+    vector<Iterator<C>> res;
     
-    template<typename C>
-    void sort(C& c) {
-        sort(c.begin(), c.end());
+    for (auto p = c.begin(); p != c.end(); ++p) {
+        if (*p == v)
+            res.push_back(p);
     }
     
-    template<typename C, typename Pred>
-    void sort(C& c, Pred p) {
-        sort(c.begin(), c.end(), p);
-    }
-    // ...
+    return res;
 }
+
+
+void test() {
+    string m {"Mary had a little lamb"};
+    for (auto p : find_all(m, 'a')) { // p is a string::iterator
+        if (*p!='a')
+            cerr << "string bug!\n";
+    }
+    
+    list<double> ld {1.1, 2.2, 3.3, 1.1};
+    for (auto p : find_all(ld, 1.1)) { // p is a list<double>::iterator
+        if (*p != 1.1)
+            cerr << "list bug!\n";
+    }
+    
+    vector<string> vs { "red", "blue", "green", "green", "orange", "green" };
+    for (auto p : find_all(vs, "red")) { // p is a vector<string>::iterator
+        if (*p != "red")
+            cerr << "vector bug!\n";
+    }
+    
+    for (auto p : find_all(vs, "green"))
+        *p = "vert";
+}
+
 
 
 int main() {
+    test();
     
     return 0;
 }
+
