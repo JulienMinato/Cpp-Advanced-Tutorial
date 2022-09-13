30aa1b809c2feae36609eb4a1f8d02fa357fbd14
Liang <SulaFelinae@gmail.com>
2022-09-12 17:21:40 -0700

    vector

--- C++ Tutorial/the Basics-Vector/main.cpp
+++ C++ Tutorial/the Basics-Vector/main.cpp
@@ -5,26 +5,49 @@
 //  Created by Liang Zhao on 8/15/22.
 #include <vector>
 #include <iostream>
-using namespace std ;
+//using namespace std ;
+//
+//int main()
+//{
+//  vector <int> vec(3,100) ;
+//
+//  cout << "Vector size: " << vec.size() << endl ;
+//  cout << "Is empty?: " << vec.empty() << endl ;
+//  cout << "First element: " << vec.at(0) << endl ;
+//
+//  vec.pop_back() ; // Remove final element.
+//  cout << "Vector size: " << vec.size() << endl ;
+//  cout << "Final element: " << vec.back() << endl;
+//
+//  vec.clear() ; // Remove all elements.
+//  cout << "Vector size: " << vec.size() << endl;
+//
+//  vec.push_back( 200 ) ; // Add an element.
+//  cout << "Vector size: " << vec.size() << endl ;
+//  cout << "First element: " << vec.front() << endl ;
+//
+//  return 0 ;
+//}
+
+
+
+//
+//Write a program to create a vector with ten int elements.
+//Using an iterator, assign each element a value that is twice its current value.
+// Test your program by printing the vector.
+//
+
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+using std::vector; using std::iterator; using std::cout;
 
 int main()
 {
-  vector <int> vec(3,100) ;
+    vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    for (auto it = v.begin(); it != v.end(); ++it) *it *= 2;
+    for (auto i : v) cout << i << " ";
 
-  cout << "Vector size: " << vec.size() << endl ;
-  cout << "Is empty?: " << vec.empty() << endl ;
-  cout << "First element: " << vec.at(0) << endl ;
-
-  vec.pop_back() ; // Remove final element.
-  cout << "Vector size: " << vec.size() << endl ;
-  cout << "Final element: " << vec.back() << endl;
-
-  vec.clear() ; // Remove all elements.
-  cout << "Vector size: " << vec.size() << endl;
-
-  vec.push_back( 200 ) ; // Add an element.
-  cout << "Vector size: " << vec.size() << endl ;
-  cout << "First element: " << vec.front() << endl ;
-
-  return 0 ;
+    return 0;
 }
